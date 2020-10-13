191258cd61b4f67ab54942ee50c5426c0e2172f9
LovelySahoo <53365601+LovelySahoo@users.noreply.github.com>
2020-10-13 14:47:21 +0530

    faculty login , forget password

---
+++ app/src/main/java/com/cutm/majorproject/cbct/FacultyForgetPasswordActivity.java
@@ -0,0 +1,14 @@
+package com.cutm.majorproject.cbct;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class FacultyForgetPasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_faculty_forget_password);
+    }
+}
---
+++ app/src/main/java/com/cutm/majorproject/cbct/Facultylogin.java
@@ -0,0 +1,43 @@
+package com.cutm.majorproject.cbct;
+
+public class Facultylogin {
+
+
+    private Long facultyID;
+    private String facultyName;
+    private String password;
+
+
+    public Facultylogin() {
+    }
+
+    public Facultylogin(Long facultyID, String facultyName, String password) {
+        this.facultyID = facultyID;
+        this.facultyName = facultyName;
+        this.password = password;
+    }
+
+    public Long getFacultyID() {
+        return facultyID;
+    }
+
+    public void setFacultyID(Long facultyID) {
+        this.facultyID = facultyID;
+    }
+
+    public String getFacultyName() {
+        return facultyName;
+    }
+
+    public void setFacultyName(String facultyName) {
+        this.facultyName = facultyName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
